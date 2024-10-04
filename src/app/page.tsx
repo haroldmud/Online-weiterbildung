61e1bd8d0ac3d0c671e7cbd4b79bfce9ef515f4f
haroldmud <haroldmudosa@gmail.com>
2024-10-04 16:16:42 +0200

    feat: implement a footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import Header from "@/components/Header";
 import Content from "@/components/content";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <section>
+    <section className="flex flex-col justify-between h-[100vh]">
+      <section>
       <Header />
       <section className="flex justify-center">
         <section className="max-w-[1000px] w-full">
           <Content />
         </section>
       </section>
+      </section>
+      <Footer />
     </section>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import Button from "../ui/Button";
+const Footer =()=> {
+  return (
+    <header className="bg-black h-12 flex justify-center py-10">
+      <div className=" max-w-[1000px] w-full relative flex items-center">
+        <div className="w-full absolute flex justify-center gap-10">
+          <a href="#" className="text-sm text-gray-400">Conditions Générales de Vente</a>
+          <a href="#" className="text-sm text-gray-400">Politique de Confidentialité</a>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Footer;
