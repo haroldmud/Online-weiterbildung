9a7d200be2315cc1ede62bd285b739139f733dd2
haroldmud <haroldmudosa@gmail.com>
2024-10-04 17:55:34 +0200

    feat: perform the first migration

---
+++ backend/prisma/migrations/20241004155406_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Formation" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "wholesalePrice" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Formation_pkey" PRIMARY KEY ("id")
+);
