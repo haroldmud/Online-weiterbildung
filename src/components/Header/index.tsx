703d1a757c5eb3b8c9a580761c70f67d8d43452f
haroldmud <haroldmudosa@gmail.com>
2024-10-04 16:29:30 +0200

    feat: implement some special feature on the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,31 @@
+"use client"
+
 import Button from "../ui/Button";
+import { useEffect, useState } from "react";
+
+
 const Header =()=> {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY < 400){
+        setShow(true);
+      }else{
+        setShow(false);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  },[])
+
   return (
-    <header className="bg-black h-12 flex justify-center">
-      <div className=" max-w-[1000px] w-full relative flex items-center">
+    <header className={`bg-black h-12 flex justify-center ${show && "sticky"}  top-0 z-50`} >
+      <div className="max-w-[1000px] w-full relative flex items-center">
         <div className="w-full absolute flex justify-end">
           <Button name="Se connecter"/>
         </div>
