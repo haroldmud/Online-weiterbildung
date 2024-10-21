e04d7509a574856963a40bfc665876729f90ed8c
haroldmud <haroldmudosa@gmail.com>
2024-10-21 17:10:53 +0200

    feat: hashing passwords fields on post and update requests

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,11 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+
+export const roundOfHashing = 10;
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
-  create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto) {
+    createUserDto.password = await bcrypt.hash(
+      createUserDto.password,
+      roundOfHashing,
+    );
+
     return this.prisma.user.create({ data: createUserDto });
   }
 
@@ -19,6 +27,12 @@ export class UsersService {
   }
 
   update(id: string, updateUserDto: UpdateUserDto) {
+    if (updateUserDto.password) {
+      updateUserDto.password = bcrypt.hashSync(
+        updateUserDto.password,
+        roundOfHashing,
+      );
+    }
     return this.prisma.user.update({ where: { id }, data: updateUserDto });
   }
 
