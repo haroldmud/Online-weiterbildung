98f50d6fe0eb0e988d64af1e7e7fb825b937fb94
haroldmud <haroldmudosa@gmail.com>
2024-10-30 12:42:47 +0200

    feat: implement zustand

--- frontend/src/zustand/store.ts
+++ frontend/src/zustand/store.ts
@@ -1,6 +1,8 @@
-// import { create } from zustand;
+import { create } from "zustand"
 
-// const useStore = create((set: any) => ({
-//   bears: 0,
-  
-// })) 
+const useStore = create((set: any) => ({
+  isLogged: false,
+  signinIn: () => set(()=> ({isLogged: true})),
+  signOut: () => set(()=> ({isLogged: false})),
+  toggle: () => set((state: {isLogged: boolean}) => ({isLogged: !state.isLogged}))
+})) 
