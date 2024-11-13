baeff9a09e5a375e7fc2110efd6e1520cffa78d6
haroldmud <haroldmudosa@gmail.com>
2024-11-13 18:13:54 +0200

    feat: NextAuth configuratin

---
+++ frontend/src/app/api/auth/[...nextAuth].ts
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+export const AuthOptions = {
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  // pages: {
+  //   signIn: "/auth/login",
+  // },
+  // callbacks: {
+  //   async signIn(user, account, profile) {
+  //     return true;
+  //   },
+  //   async redirect(url, baseUrl) {
+  //     return url.startsWith(baseUrl) ? url : baseUrl;
+  //   },
+  //   async session(session, user) {
+  //     return session;
+  //   },
+  //   async jwt(token, user, account, profile, isNewUser) {
+  //     return token;
+  //   },
+  }
+
+  export default NextAuth(AuthOptions);
