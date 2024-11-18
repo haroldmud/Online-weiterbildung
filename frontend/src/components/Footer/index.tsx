6304c4f31c70b057a1b3e121903effecd34cfdae
haroldmud <haroldmudosa@gmail.com>
2024-11-18 11:36:49 +0200

    feat: implement the email template for verification

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,13 +1,13 @@
 const Footer =()=> {
   return (
-    <header className="bg-black h-12 flex justify-center py-10">
+    <section className="bg-black h-12 flex justify-center py-10">
       <div className=" max-w-[1000px] w-full relative flex items-center">
         <div className="w-full absolute flex justify-center gap-10">
           <a href="#" className="text-sm text-gray-400">Conditions Générales de Vente</a>
           <a href="#" className="text-sm text-gray-400">Politique de Confidentialité</a>
         </div>
       </div>
-    </header>
+    </section>
   );
 }
 
